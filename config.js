758c05b03f290be6adddec896b092cd2197e74f7
Luke Ritchie <loritchie@gmail.com>
2018-02-06 12:27:26 -0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-
-	consumer_key:         'VCXZsErpJOyYmNHobMUO6rKvi',
-	consumer_secret:      'GgtZdyAafDlb1xJUG1tSIuADBH8dkYkRYXXGZYfpKstMwMXipw',
-	access_token:         '5930022-6ACJ641kymZDlDJy8R3yjZp2oLqMCyrLVVUyn2rWFH',
-	access_token_secret:  'F2pIfo5K7X13IZ5qDErksK2cfy1i56astzdMx7QuivvQD'
-
-}
